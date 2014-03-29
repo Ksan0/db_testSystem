4d2c4b747724f99ddf376a9d2d6bf3c38132311c
LifeMoroz <delfin1995@yandex.ru>
2014-03-29 23:09:05 +0400

    Scripts and CSS

---
+++ resources/javascripts/script.js
@@ -0,0 +1,40 @@
+$(function(){
+    $("a.dropdown-toggle").click(function(e) {
+      $(this).parent().toggleClass('open');
+      e.preventDefault();
+    });
+
+    $("#ajax-check-sql").click(function() {
+        var form = $('.contact-form');
+        $.post(
+            "/test/ajax/attempt-sql/", //url
+            form.serialize(),
+            function(data) {      //success method
+                $('#messages').html(data);
+            }
+        );
+        return false;
+    });
+
+
+//  $('#ajax-check-sql').click(function(e) {
+//      e.preventDefault();
+//      $.ajax({
+//          type: "POST",
+//          url: "/test/ajax/attempt-sql/",
+//          dataType: "json",
+//          data: {
+//              'csrfmiddlewaretoken':$( "#csrfmiddlewaretoken" ).val(),
+//              'sql': $(this).serialize()
+//          }
+//          ,
+//          success: function(data) {
+//              $('p').html('ok');
+//              alert(1);
+//          },
+//          error: function(data) {
+//
+//          }
+//      });
+//  })
+})
