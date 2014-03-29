5b223856edfa76fca7c516400615e2b141c84a6d
Ksan0 <kgfq@mail.ru>
2014-03-29 20:14:18 +0400

    init

--- db_testSystem/admin.py
+++ db_testSystem/admin.py
@@ -4,16 +4,9 @@ from django.contrib.auth.models import Group
 from models import *
 
 
-admin.site.unregister(User)
+#admin.site.unregister(User)
 admin.site.unregister(Group)
 
-
-class NewRKModel(RK):
-    def __init__(self):
-        self.questions = Question.objects.filter(rk=self)
-
-    class Meta:
-        proxy = True
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('title', 'rk', 'is_active')
@@ -24,4 +17,4 @@ admin.site.register(Question, QuestionAdmin)
 class RKAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active')
     list_editable = ('is_active',)
-admin.site.register(NewRKModel, RKAdmin)
+admin.site.register(RK, RKAdmin)
