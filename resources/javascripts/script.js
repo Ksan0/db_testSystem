d0722e6be760058a41d1baee233a5a6a05263718
Ksan0 <kgfq@mail.ru>
2014-03-30 20:39:28 +0400

    upd 37

--- resources/javascripts/script.js
+++ resources/javascripts/script.js
@@ -10,7 +10,7 @@ $(function(){
         var form = $('.contact-form');
 		var get = parseGetParams();
         $.post(
-            "ajax/answer/?type=answer&testid=get.testid&queid=get.queid", //url
+            "/test_answer/?testid=get.testid&queid=get.queid", //url
             form.serialize(),
             function(data) {      //success method
                 $('#messages').html(data);
