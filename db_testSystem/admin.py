e8b546cad0b36c2f53ba4399ee6f11c69e838527
Ksan0 <kgfq@mail.ru>
2014-03-30 15:41:35 +0400

    upd 26

--- db_testSystem/admin.py
+++ db_testSystem/admin.py
@@ -1,26 +1,33 @@
 # coding: utf-8
+from django.conf.urls import patterns
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.forms import forms
+from django.http import HttpResponseRedirect
 from models import *
 
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
-class SessionQuestionsAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(SessionQuestions, SessionQuestionsAdmin)
 
+class TryingInline(admin.StackedInline):
+    model = Attempt
+    extra = 0
 
-class UserSessionAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(UserSession, UserSessionAdmin)
+    def get_formset(self, request, obj=None, **kwargs):
+        self.object = Attempt.objects.filter(user=obj)
+        return super(TryingInline, self).get_formset(request, obj, **kwargs)
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'is_active')
     list_editable = ('is_active',)
     fields = ('username', 'first_name', 'last_name', 'is_active')
+    inlines = [TryingInline]
+
+    # def change_view(self, request, object_id, form_url='', extra_context=None):
+    #    return HttpResponseRedirect('/abc')
 admin.site.register(User, UserAdmin)
 
 
@@ -36,8 +43,8 @@ class RKAdmin(admin.ModelAdmin):
     list_editable = ('is_active',)
 admin.site.register(RK, RKAdmin)
 
-
+"""
 class AttemptAdmin(admin.ModelAdmin):
     list_display = ('user', 'rk', 'used')
-
 admin.site.register(Attempt, AttemptAdmin)
+"""
