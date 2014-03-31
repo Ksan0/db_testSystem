634306f58d28ed22d103ebe0953af2ec32315cf4
root <root@debian.ispmgr5.template>
2014-03-31 18:39:42 +0400

    upd 70

--- db_testSystem/wsgi.py
+++ db_testSystem/wsgi.py
@@ -7,8 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+
+import sys, os
+sys.path.append('/var/www/ksan/data/www/db_testSystem')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db_testSystem.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+"""
+
+import os, sys
+
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+sys.path.append('/var/www/ksan/data/www/cimg.ru')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+"""
