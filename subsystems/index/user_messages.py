d6bbfc6770f81dcaccd874887977663536998e10
Ksan0 <kgfq@mail.ru>
2014-04-02 18:18:15 +0400

    upd 95

--- subsystems/index/user_messages.py
+++ subsystems/index/user_messages.py
@@ -8,13 +8,3 @@ SESSION_CLOSED = 'session closed'
 LOGIN_FAILED = 'login failed'
 DISABLED_USER = 'disabled user'
 ANOTHER_TEST_RUNNING = 'another test running'
-
-"""class UserMessage():
-    def __init__(self, msg, type, url):
-        x
-
-class SuccessUserMessage():
-    def __init__(self, msg):
-        super(SuccessUserMessage, self).__init__(msg)
-        self.type =
-        """
