d07c9ab982aa80379df084ca32331e54a7dd10fc
Ksan0 <kgfq@mail.ru>
2014-04-02 17:08:06 +0400

    easy install 1

--- db_testSystem/wsgi.py
+++ db_testSystem/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 
 import sys, os
-sys.path.append('/var/www/ksan/data/www/db_testSystem')
+
+# sys.path.append('/var/www/ksan/data/www/db_testSystem')
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db_testSystem.settings")
+
+BASE_DIR = os.getcwd() + '/..'
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db_testSystem.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
