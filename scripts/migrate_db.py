6d42c417f7de874b88ea5ce3c67d3ded373eabd4
tpsql <tpadmin@sql.sql.openrise.org>
2014-04-05 20:36:36 +0400

    migrate problem

--- scripts/migrate_db.py
+++ scripts/migrate_db.py
@@ -4,24 +4,35 @@ import string
 
 
 def migrate_db():
-    from django.contrib.auth.models import User
+    from db_testSystem.models import Question
     from subsystems.db_raw_sql_works.DB import Review
 
     db_settings = {
         'HOST_NAME': '',
-        'DB_NAME': 'db_testSystem',
-        'USER_NAME': 'testUser',
-        'USER_PASSWORD': 'qwe',
+        'DB_NAME': 'mailtp',
+        'USER_NAME': 'root',
+        'USER_PASSWORD': 'eCR7qeJS',
         'DB_CHARSET': 'utf8'
     }
 
     reviewer = Review(db_settings)
-    count = int( reviewer.select('SELECT COUNT(*) FROM knowledge_test_question')['records'] )
+    
+    # questions migrate
+    """
+    count = int( reviewer.select('SELECT COUNT(*) FROM knowledge_test_question')['records'][0]['COUNT(*)'] )
     for i in range(count):
         query = ('SELECT * FROM knowledge_test_question LIMIT {0},1').format(i)
-        que = reviewer.select(query)['records']
-        print que
+        que = reviewer.select(query)['records'][0]
+        Question.objects.create(title=que['title'], description=que['description'], answer=que['answer'], is_active=False)
+    """
 
+    # users migrate
+    count = int( reviewer.select('SELECT COUNT(*) FROM auth_user')['records'][0]['COUNT(*)'] )
+    for i in range(count):
+        query = ('SELECT * FROM knowledge_test_question LIMIT {0},1').format(i)
+        que = reviewer.select(query)['records'][0]
+        newpass = ''.join([random.choice(string.ascii_uppercase) for _ in xrange(12)])
+                
     return 0
 
 
